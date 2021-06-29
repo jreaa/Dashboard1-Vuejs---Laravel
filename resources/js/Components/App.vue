552c006e004203d324af30e73d64c8de76f9e881
Jose Era <jorfgfe@gmail.com>
2021-06-28 22:54:55 -0400

    first commit

---
+++ resources/js/Components/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+
+		<!--end::Header Mobile-->
+		<div class="d-flex flex-column flex-root">
+			<!--begin::Page-->
+			<div class="d-flex flex-row flex-column-fluid page">
+				<Aside></Aside>
+				<!--begin::Wrapper-->
+				<div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
+					<Header></Header>
+					<!--begin::Content-->
+					<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+
+						<!--begin::Entry-->
+						<div class="d-flex flex-column-fluid">
+                             <main class="container" style="padding-bottom: 50px;">
+                                
+                            </main>
+						</div>
+						<!--end::Entry-->
+					</div>
+					<!--end::Content-->
+                    <Footer></Footer>
+				</div>
+				<!--end::Wrapper-->
+			</div>
+			<!--end::Page-->
+		</div>
+		<!--end::Main-->
+    </div>
+</template>  
+
+<script>
+import Aside from '../Layouts/Aside.vue'
+import Header from '../Layouts/Header.vue'
+import Footer from '../Layouts/Footer.vue'
+export default {
+    components: {
+        Aside,
+        Header,
+        Footer
+    }
+}
+</script>
---
+++ resources/js/Layouts/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    	<!--begin::Footer-->
+					<div class="footer bg-white py-4 d-flex flex-lg-column" id="kt_footer" style="position: fixed;bottom: 0px;height: 40px;width: 100%;margin-top:20px">
+						<!--begin::Container-->
+						<div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
+							<!--begin::Copyright-->
+							<div class="text-dark order-2 order-md-1">
+								<span class="text-muted font-weight-bold mr-2">2021©</span>
+								<a href="http://keenthemes.com/metronic" target="_blank" class="text-dark-75 text-hover-primary">Bapp | Jose Era</a>
+							</div>
+							<!--end::Copyright-->
+					
+						</div>
+						<!--end::Container-->
+					</div>
+					<!--end::Footer-->
+  </div>
+</template>
